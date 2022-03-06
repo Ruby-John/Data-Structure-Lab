ba56b7a183999917050edbbd08d84b6163b0fb0d
Ruby-John <90848712+Ruby-John@users.noreply.github.com>
2022-03-06 04:33:04 -0500

    Create output-linkpoly.c

---
+++ output-linkpoly.c
@@ -0,0 +1,20 @@
+Polynomial 1--
+No.of terms:3
+coeff.:6
+exp.:4
+coeff.:5
+exp.:2
+coeff.:7
+exp.:0
+Polynomial 2--
+No.of terms:3
+coeff.:7
+exp.:4
+coeff.:6
+exp.:2
+coeff.:2
+exp.:0
+Polynomial 1:6x^4+5x^2+7x^0
+Polynomial 2:7x^4+6x^2+2x^0
+Result:13x^4+11x^2+9x^0
+--------------------------------
