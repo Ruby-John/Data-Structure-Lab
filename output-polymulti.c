f96d394f2afc8cda1f859def8623ae2e4f1e29c0
Ruby-John <90848712+Ruby-John@users.noreply.github.com>
2022-03-06 04:39:16 -0500

    Create output-polymulti.c

---
+++ output-polymulti.c
@@ -0,0 +1,20 @@
+Polynomial 1--
+No.of terms:3
+coeff.:6
+exp.:4
+coeff.:5
+exp.:2
+coeff.:7
+exp.:0
+Polynomial 2--
+No.of terms:3
+coeff.:3
+exp.:3
+coeff.:4
+exp.:2
+coeff.:3
+exp.:0
+Polynomial 1:6x^4+5x^2+7x^0
+Polynomial 2:3x^3+4x^2+3x^0
+Result:18x^7+24x^6+15x^5+38x^4+21x^3+43x^2+21x^0
+--------------------------------
